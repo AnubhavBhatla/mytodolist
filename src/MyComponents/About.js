26a38144ed41a0161ca6225de2bc764110a1b215
Anubhav Bhatla <76862682+AnubhavBhatla@users.noreply.github.com>
2022-05-20 14:52:32 +0530

    added source code

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This is an about component
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, consequuntur. Corporis nihil quas culpa, adipisci expedita fuga suscipit delectus. Quasi possimus quidem aut reiciendis officia, delectus dolores corrupti a. Veritatis!
+        </p>
+    </div>
+  )
+}
